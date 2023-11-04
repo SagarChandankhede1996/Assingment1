437a596303bb325eb1db733c0b5d2a19a52e2eb6
SagarChandankhede1996 <148791586+SagarChandankhede1996@users.noreply.github.com>
2023-11-04 19:20:36 +0530

    new commit updated one

--- w2Assignment4/A4.java
+++ w2Assignment4/A4.java
@@ -1,13 +1,20 @@
 package w2Assignment4;
 class MyRunnable implements Runnable{
 	private String str;
-	public MyRunnable(String s){
+	private String[] c;
+
+	public MyRunnable(String s,String[] course){
+		
 		this.str=s;
+		this.c=course;
 	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println(this.str);
+		for(String obj : this.c) {
+			System.out.println(this.str+obj);
+		}
+		
 	}
 
 }
@@ -16,18 +23,16 @@ public class A4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] course = {"Java", "J2EE", "Spring"};
-		for(String obj : course) {
-			MyRunnable r1=new MyRunnable("Thread 1 : "+obj);
-			Thread t1=new Thread(r1);
-			t1.start();
-			MyRunnable r2=new MyRunnable("Thread 2 : "+obj);
-			Thread t2=new Thread(r2);
-			t2.start();
-			MyRunnable r3=new MyRunnable("Thread 3 : "+obj);
-			Thread t3=new Thread(r3);
-			t3.start();
-		}
 
+		MyRunnable r1=new MyRunnable("Thread 1 : ", course);
+		Thread t1=new Thread(r1);
+		MyRunnable r2=new MyRunnable("Thread 2 : ", course);
+		Thread t2=new Thread(r2);
+		MyRunnable r3=new MyRunnable("Thread 3 : ", course);
+		Thread t3=new Thread(r3);
+		t1.start();
+		t2.start();
+		t3.start();
 	}
 
 }
